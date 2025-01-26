a1fdb030dee3681a1c124226f5522d52b18c48ae
arthur <arthurdomiciano46@gmail.com>
2025-01-26 16:17:56 -0500

    js

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
-$(document).ready(function(){
-    $('header button').click(function(){
-        alert('expand form');
+$(document).ready(function() {
+    $('header button').click(function() {
+        $('form').slideDown();
     })
+    
+
+    $('#cancel-button').click(function() {
+        $('form').slideUp();
+    }) 
+
+    
 
     $('form').on('submit', function(e) {
-        console.log("submit");
         e.preventDefault();
+        const newImageLink = $('#link-new-image').val();
+        const NewItem = $('<li></li>');
+        $(`<img src="${newImageLink}" />`).appendto(NewItem);
+        $(`<div class="overlay-image-link"> <a href="${newImageLink}" target="_blank" title="ver imagen en tamanho real" </a> </div>`).appendto(NewItem);
+        $(NewItem).appendto('ul');
     })
 })
